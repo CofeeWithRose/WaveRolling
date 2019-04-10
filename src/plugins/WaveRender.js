2e866434f90b005a0db83129c4d4f2f32a7741ca
PH <1836071395@qq.com>
2019-04-10 15:24:42 +0800

    make render clear

--- src/plugins/WaveRender.js
+++ src/plugins/WaveRender.js
@@ -12,7 +12,7 @@ export class WaveRender {
         this._color = color || '#000000';
         this._canvas = document.createElement('canvas');
         this._context = this._canvas.getContext('2d');
-        this._canvas.width = Math.max (window.screen.width, window.screen.height); 
+        this._canvas.width = devicePixelRatio * Math.max (window.screen.width, window.screen.height); 
         this._canvas.style.width = '100%';
         this._canvas.style.height = '100%';
         this._canvas.height = container.clientHeight;
@@ -28,6 +28,7 @@ export class WaveRender {
     reset(){
         this._context.beginPath();
         this._context.strokeStyle = this._color;
+        this._context.lineWidth = 2;
         this._context.moveTo(0, this._halfHeight);
         this._context.lineTo(this._canvas.width, this._halfHeight);
         this._context.stroke()
@@ -37,16 +38,17 @@ export class WaveRender {
         const floatArrayData = audioBuffer.getChannelData(0);
         const startX = this._canvas.width * startPercent;
         const endX = this._canvas.width * endPercent;
-        const stepnum = Math.max (Math.floor( devicePixelRatio * 100 *  (endX - startX) ) , 10);
+        const stepnum = Math.max (Math.floor( 100 *  (endX - startX) ) , 10);
         const stepIndex = Math.max( Math.floor(floatArrayData.length/stepnum), 1);
         const stepX = ( endX - startX )/ stepnum;
         this._context.beginPath();
+        this._context.strokeStyle = this._color;
         this._context.strokeStyle = this._color;
         for(let i = 0; i < stepnum; i++){
             const x = startX+ stepX*i;
             this._context.lineTo( x,  this._halfHeight + this._halfHeight*floatArrayData[i * stepIndex]);
         }
-        this._context.stroke()
+        this._context.stroke();
     }
 
     clear(){
