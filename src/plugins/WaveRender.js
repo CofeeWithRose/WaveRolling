d7156685aabc3efa68aa33b078c803c3dd93156d
PH <1836071395@qq.com>
2019-04-11 14:29:16 +0800

    improve quality of  image

--- src/plugins/WaveRender.js
+++ src/plugins/WaveRender.js
@@ -5,7 +5,7 @@ export class WaveRender {
     /**
      * initial the canvas.
      * @param { HTMLElement } container 
-     * @param { { color: Color | Array<Color> } } options 
+     * @param { { color: Color | Array<Color>, scalable } } options 
      */
     constructor(container, options ){
         const { color } = options ||{};
@@ -13,11 +13,12 @@ export class WaveRender {
         this._canvas = document.createElement('canvas');
         this._context = this._canvas.getContext('2d');
         this._canvas.width = devicePixelRatio * Math.max (window.screen.width, window.screen.height); 
+        // this._canvas.width = 130080zzz
         this._canvas.style.width = '100%';
         this._canvas.style.height = '100%';
         this._canvas.height = container.clientHeight;
         this._halfHeight = this._canvas.height * 0.5;
-        this._color = this._createColor(color);
+        this._color = color;
         this.reset();
         container.appendChild(this._canvas);
     }
@@ -25,13 +26,13 @@ export class WaveRender {
     // _context;
     // _canvas;
     // _halfHeight;
-    _createColor(color){
+    _setColor(){
         let result;
-        if(color instanceof Array){
+        if(this._color instanceof Array){
             result = this._context.createLinearGradient(0, this._halfHeight,this._canvas.width, this._halfHeight);
-            color.forEach( ( item, index ) => {
+            this._color.forEach( ( item, index ) => {
                 if('string' === typeof item){
-                    result.addColorStop(index/(color.length-1), item);
+                    result.addColorStop(index/(this._color.length-1), item);
                 }else{
                     const {offset, value} = item;
                     result.addColorStop(offset, value);
@@ -39,11 +40,12 @@ export class WaveRender {
                 
             });
         }else{
-            result = color;
+            result = this._color;
         }
-        return result;
+        this._context.strokeStyle = result;
     }
     reset(){
+        this._setColor();
         this._context.beginPath();
         this._context.strokeStyle = this._color;
         this._context.lineWidth = 2;
@@ -55,19 +57,48 @@ export class WaveRender {
         // this._context.moveTo(0, this._halfHeight);
     }
 
+    // render(audioBuffer, startPercent, endPercent){
+    //     const floatArrayData = audioBuffer.getChannelData(0);
+    //     const startX = this._canvas.width * startPercent;
+    //     const endX = this._canvas.width * endPercent;
+    //     const stepnum = Math.max (Math.floor( 100 *  (endX - startX) ) , 10);
+    //     const stepIndex = Math.max( Math.floor(floatArrayData.length/stepnum), 1);
+    //     const stepX = ( endX - startX )/ stepnum;
+    //     this._context.beginPath();
+    //     this._context.strokeStyle = this._color;
+
+    //     for(let i = 0; i < stepnum; i++){
+    //         const x = startX+ stepX*i;
+    //         this._context.lineTo( x,  this._halfHeight + this._halfHeight*floatArrayData[i * stepIndex]);
+    //     }
+    //     this._context.stroke();
+    // }
+
     render(audioBuffer, startPercent, endPercent){
+        
         const floatArrayData = audioBuffer.getChannelData(0);
-        const startX = this._canvas.width * startPercent;
-        const endX = this._canvas.width * endPercent;
-        const stepnum = Math.max (Math.floor( 100 *  (endX - startX) ) , 10);
+        if(!this._isReset){
+            this._canvas.width = Math.min(30766,floatArrayData.length * 1/(endPercent - startPercent));
+            this._context = this._canvas.getContext('2d');
+            this.reset();
+            this._isReset = true;
+        }
+        const startX = Math.floor(this._canvas.width * startPercent);
+        const endX = Math.ceil(this._canvas.width * endPercent);
+        const stepnum = Math.max (Math.floor( 2 * (endX - startX) ) , 1);
         const stepIndex = Math.max( Math.floor(floatArrayData.length/stepnum), 1);
         const stepX = ( endX - startX )/ stepnum;
         this._context.beginPath();
         this._context.strokeStyle = this._color;
 
         for(let i = 0; i < stepnum; i++){
+            const sample = [];
+            for( let j=0; j< stepIndex; j++ ){
+                sample.push(Math.abs(floatArrayData[j + i]));
+            }
             const x = startX+ stepX*i;
-            this._context.lineTo( x,  this._halfHeight + this._halfHeight*floatArrayData[i * stepIndex]);
+            const y = sample.reduce( (pre, cur) => pre + cur)/sample.length;
+            this._context.lineTo( x,  this._halfHeight +  this._halfHeight *  +  y * (i%2 *2 -1) );
         }
         this._context.stroke();
     }
