e1181c23adc2e0caa1ce3d4ccf302bda17646792
PH <1836071395@qq.com>
2019-04-14 20:28:22 +0800

    add ts resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 
 const commonConfig = {
 
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
