62c8f2d16f61419bbd45938dc187a6b3dd5070f6
PH <1836071395@qq.com>
2019-04-25 13:36:04 +0800

    up version

--- release/util/event/IEmiter.d.ts
+++ release/util/event/IEmiter.d.ts
@@ -4,5 +4,6 @@ export interface EventMap {
 export interface IEmiter {
     addListener(name: string | number, fun: (...params: Array<any>) => void): void;
     removeListener(name: string | number, fun: (...params: Array<any>) => void): void;
+    removeListeners(name: string | number): void;
     emit(name: string | number, ...params: Array<any>): void;
 }
